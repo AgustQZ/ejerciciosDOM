71338bf2d033a2f86023eaf0927b6a75720928f5
Agustin Quiceno Z <san.agustin20.aqz@gmail.com>
2022-03-22 05:14:25 -0500

    script para incluir elementos html por separado terminado

---
+++ ajax_ejercicios/sitio-ajax.js
@@ -0,0 +1,46 @@
+const d = document,
+    $main = d.querySelector("main");
+
+const getHTML = (options) => {
+    let { url, success, err } = options;
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange", (e) => {
+        if (xhr.readyState !== 4) return;
+
+        if (xhr.status >= 200 && xhr.status < 300) {
+            let html = xhr.responseText;
+            success(html);
+        } else {
+            let message = xhr.statusText || "Ocurrio un error";
+            err(`${xhr.status}: ${message}`);
+        }
+    });
+
+    xhr.open("GET", url);
+    xhr.setRequestHeader("Content-type", "text/html; charset= utf-8");
+    xhr.send();
+};
+
+d.addEventListener("DOMContentLoaded", (e) => {
+    getHTML({
+        url: "assets/home.html",
+        success: (html) => ($main.innerHTML = html),
+        error: (err) => {
+            $main.innerHTML = `<h1>${err}</h1>`;
+        },
+    });
+});
+
+d.addEventListener("click", (e) => {
+    if (e.target.matches(".menu a")) {
+        e.preventDefault();
+        getHTML({
+            url: e.target.href,
+            success: (html) => ($main.innerHTML = html),
+            error: (err) => {
+                $main.innerHTML = `<h1>${err}</h1>`;
+            },
+        });
+    }
+});
